17c8ac59fcafd829da6c9f6700577c52539cd6e4
Pashalim123 <pashalimkg@gmail.com>
2025-07-17 19:06:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reply_keyboard = [['Забронировать столик'], ['Меню'], ['Оставить отзыв']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    await update.message.reply_text(
+        f"Привет, {update.effective_user.first_name}! Добро пожаловать в NOX Nightclub!",
+        reply_markup=markup
+    )
+
+app = ApplicationBuilder().token("YOUR_TELEGRAM_BOT_TOKEN").build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
